79db49f45fe2b5cf1ad7b2dc81facc9b313b02ae
Prizzz <rob_243248@mail.ru>
2022-08-01 23:56:38 +0400

    feat: add display of the number tickets and prices

--- museum/js/booking.js
+++ museum/js/booking.js
@@ -1,5 +1,24 @@
 "use strict";
 
+const BASIC_PRICE = 20;
+const SENIOR_PRICE = 10;
+
+let basicAmount = Number(document.querySelector(".basic").value),
+  seniorAmount = Number(document.querySelector(".senior").value),
+  typeContainer = document.querySelector(".tickets-type");
+
+const priceTotal = document.querySelector(".price span"),
+  modalPriceTotal = document.querySelector(".receipt-total-price"),
+  tickets = document.querySelector(".tickets-amount"),
+  modalEntryBasicAmount = document.querySelector(".booking-entry_basic"),
+  modalEntrySeniorAmount = document.querySelector(".booking-entry_senior"),
+  modalOverviewBasicAmount = document.querySelector(".booking-overview_basic"),
+  modalOverviewSeniorAmount = document.querySelector(
+    ".booking-overview_senior"
+  ),
+  modalOverviewBasicPrice = document.querySelector(".booking-price_basic"),
+  modalOverviewSeniorPrice = document.querySelector(".booking-price_senior");
+
 function modalWindow() {
   const modal = document.querySelector(".booking"),
     close = document.querySelector(".close"),
@@ -19,3 +38,35 @@ function modalWindow() {
   });
 }
 modalWindow();
+
+function ticketsCalc() {
+  basicAmount = Number(document.querySelector(".basic").value);
+  seniorAmount = Number(document.querySelector(".senior").value);
+
+  const priceValue = BASIC_PRICE * basicAmount + SENIOR_PRICE * seniorAmount;
+
+  priceTotal.textContent = priceValue;
+  modalPriceTotal.textContent = priceValue + " €";
+  modalEntryBasicAmount.value = basicAmount;
+  modalEntrySeniorAmount.value = seniorAmount;
+  modalOverviewBasicAmount.textContent = basicAmount;
+  modalOverviewSeniorAmount.textContent = seniorAmount;
+  modalOverviewBasicPrice.textContent = basicAmount * BASIC_PRICE + " €";
+  modalOverviewSeniorPrice.textContent = seniorAmount * SENIOR_PRICE + " €";
+}
+
+tickets.addEventListener("click", ticketsCalc);
+
+function getCheckedRadioValue(name) {
+  let elements = document.getElementsByName(name);
+
+  for (let i = 0; i < elements.length; ++i)
+    if (elements[i].checked) price = elements[i].value;
+
+  priceTotal.textContent =
+    BASIC_PRICE * basicAmount + SENIOR_PRICE * seniorAmount;
+}
+
+function checkType() {
+  getCheckedRadioValue("radioInput");
+}
--- museum/js/main.js
+++ museum/js/main.js
@@ -8,34 +8,3 @@ import "./videoSlider.js";
 import "./gallery.js";
 import "./booking.js";
 import "./map.js";
-
-/*tickets-calc*/
-
-let basicAmount = document.querySelector(".basic").value,
-  seniorAmount = document.querySelector(".senior").value,
-  typeContainer = document.querySelector(".tickets-type");
-
-let priceTotal = document.querySelector(".price span");
-let price = 20;
-
-priceTotal.textContent = +price * +basicAmount + (+price / 2) * +seniorAmount;
-const tickets = document.querySelector(".tickets-amount");
-
-tickets.addEventListener("click", () => {
-  basicAmount = document.querySelector(".basic").value;
-  seniorAmount = document.querySelector(".senior").value;
-  priceTotal.textContent = +price * +basicAmount + (+price / 2) * +seniorAmount;
-});
-
-function checkType() {
-  getCheckedRadioValue("radioInput");
-}
-
-function getCheckedRadioValue(name) {
-  let elements = document.getElementsByName(name);
-
-  for (let i = 0; i < elements.length; ++i)
-    if (elements[i].checked) price = elements[i].value;
-
-  priceTotal.textContent = +price * +basicAmount + (+price / 2) * +seniorAmount;
-}
