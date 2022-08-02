f30a9aad294661a5519cf81b6dfbab6121fb2af3
Prizzz <rob_243248@mail.ru>
2022-08-02 23:26:32 +0400

    feat: add choose ticket type

--- museum/js/booking.js
+++ museum/js/booking.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const BASIC_PRICE = 20;
-const SENIOR_PRICE = 10;
+let BASIC_PRICE = 20;
+let SENIOR_PRICE = BASIC_PRICE / 2;
 
 let basicAmount = Number(document.querySelector(".basic").value),
   seniorAmount = Number(document.querySelector(".senior").value),
@@ -40,19 +40,20 @@ function modalWindow() {
 }
 modalWindow();
 
-function ticketsCalc(e) {
-  const targetClassName = e.target.className;
-
-  if (targetClassName === "ticket-input") {
-    basicAmount = Number(document.querySelector(".basic").value);
-    seniorAmount = Number(document.querySelector(".senior").value);
-    modalEntryBasicAmount.value = basicAmount;
-    modalEntrySeniorAmount.value = seniorAmount;
-  } else if (targetClassName === "modal-input") {
-    basicAmount = Number(modalEntryBasicAmount.value);
-    seniorAmount = Number(modalEntrySeniorAmount.value);
-    document.querySelector(".basic").value = basicAmount;
-    document.querySelector(".senior").value = seniorAmount;
+function ticketsCalc(e = null) {
+  if (e) {
+    const targetClassName = e.target.className;
+    if (targetClassName === "ticket-input") {
+      basicAmount = Number(document.querySelector(".basic").value);
+      seniorAmount = Number(document.querySelector(".senior").value);
+      modalEntryBasicAmount.value = basicAmount;
+      modalEntrySeniorAmount.value = seniorAmount;
+    } else if (targetClassName === "modal-input") {
+      basicAmount = Number(modalEntryBasicAmount.value);
+      seniorAmount = Number(modalEntrySeniorAmount.value);
+      document.querySelector(".basic").value = basicAmount;
+      document.querySelector(".senior").value = seniorAmount;
+    }
   }
 
   const priceValue = BASIC_PRICE * basicAmount + SENIOR_PRICE * seniorAmount;
@@ -68,16 +69,39 @@ function ticketsCalc(e) {
 tickets.addEventListener("click", ticketsCalc);
 modalEntryInputs.addEventListener("click", ticketsCalc);
 
-function getCheckedRadioValue(name) {
-  let elements = document.getElementsByName(name);
+const selectType = document.querySelector(".type-select");
+const ticketsType = document.querySelector(".tickets-type");
+const ticketTypeContainer = document.querySelector(".info-type");
 
-  for (let i = 0; i < elements.length; ++i)
-    if (elements[i].checked) price = elements[i].value;
+function getCheckedRadioValue(e) {
+  const elements = document.getElementsByName("radioInput");
 
-  priceTotal.textContent =
-    BASIC_PRICE * basicAmount + SENIOR_PRICE * seniorAmount;
+  if (e.target.className === "type-select") {
+    const value = selectType.value;
+    for (let i = 0; i < elements.length; ++i) {
+      if (elements[i].value == value) elements[i].checked = true;
+    }
+  }
+
+  for (let i = 0; i < elements.length; ++i) {
+    if (elements[i].checked) {
+      BASIC_PRICE = Number(elements[i].value);
+      SENIOR_PRICE = BASIC_PRICE / 2;
+      ticketTypeContainer.innerText = elements[i].parentElement.innerText;
+
+      selectType.value = elements[i].value;
+    }
+  }
+  ticketsCalc();
+
+  const amountList = document.querySelector(".amount-list");
+  const receiptListBasic = document.querySelector(".receipt-list-type_basic");
+  const receiptListSenior = document.querySelector(".receipt-list-type_senior");
+
+  amountList.innerHTML = `<p>Basic 18+ (${BASIC_PRICE} €)</p> <p>Senior 65+ (${SENIOR_PRICE} €)</p>`;
+  receiptListBasic.textContent = `Basic (${BASIC_PRICE} €)`;
+  receiptListSenior.textContent = `Senior (${SENIOR_PRICE} €)`;
 }
 
-function checkType() {
-  getCheckedRadioValue("radioInput");
-}
+ticketsType.addEventListener("click", getCheckedRadioValue);
+selectType.addEventListener("click", getCheckedRadioValue);
